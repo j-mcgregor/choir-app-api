d087a6ee28f77292d8f1144250dc45ef8e02f263
patchyj <jackjwmcgregor@gmail.com>
2020-02-11 13:16:07 -0500

    renamed bcryptJS to bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcryptJS');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const User = require('../models/user');
